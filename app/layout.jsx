08f58bc21dcf45506d0fc221b7e46de900a06f0d
Cifer0101 <l.kakabadze2020@gmail.com>
2024-02-27 23:00:00 +0400

    Components: InfoBoxes, InfoBox, Footer, Hero

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import "@/assets/styles/globals.css"
 import Navbar from "@/components/Navbar"
+import Footer from "@/components/Footer"
 
 export const metadata = {
   title: "PropertyMarketplace | Find The Perfect Rental",
@@ -13,6 +14,7 @@ const MainLayout = ({ children }) => {
       <body>
         <Navbar />
         <div>{children}</div>
+        <Footer />
       </body>
     </html>
   )
