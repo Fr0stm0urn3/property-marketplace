90c33deb2eeee6d395f43b73c3cebd9ed89c434b
Cifer0101 <l.kakabadze2020@gmail.com>
2024-02-27 23:56:01 +0400

    Properties Page, Property Card Dynamic Data

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,5 +1,3 @@
-"use client"
-
 const PropertyPage = () => {
   return (
     <>
--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,8 +1,21 @@
+import properties from "@/properties.json"
+import PropertyCard from "@/components/PropertyCard"
+
 const PropertiesPage = () => {
   return (
-    <div>
-      <h1>PropertiesPage</h1>
-    </div>
+    <section className="px-4 py-6">
+      <div className="container-xl lg:container m-auto px-4 py-6">
+        {properties.length === 0 ? (
+          <p>No properties found</p>
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {properties.map((property) => {
+              return <PropertyCard property={property} key={property._id} />
+            })}
+          </div>
+        )}
+      </div>
+    </section>
   )
 }
 
