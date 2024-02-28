ec10f44597bb480035dc80abcae699891f5b6090
Cifer0101 <l.kakabadze2020@gmail.com>
2024-02-29 02:37:17 +0400

    Cloudinary image upload & property image display

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -8,6 +8,7 @@ import Link from "next/link"
 import { FaArrowLeft } from "react-icons/fa"
 import PropertyDetails from "@/components/PropertyDetails"
 import Spinner from "@/components/Spinner"
+import PropertyImages from "@/components/PropertyImages"
 
 const PropertyPage = () => {
   const { id } = useParams()
@@ -140,6 +141,7 @@ const PropertyPage = () => {
               </div>
             </div>
           </section>
+          <PropertyImages images={property.images} />
         </>
       )}
     </>
