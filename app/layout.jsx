cd9380659fdd93618e6fb8e46cf2bef5b3bcfb08
Cifer0101 <l.kakabadze2020@gmail.com>
2024-02-28 20:12:00 +0400

    Next Auth & Provider Setup. Session provider wrapper

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import "@/assets/styles/globals.css"
 import Navbar from "@/components/Navbar"
 import Footer from "@/components/Footer"
+import AuthProvider from "@/components/AuthProvider"
 
 export const metadata = {
   title: "PropertyMarketplace | Find The Perfect Rental",
@@ -10,13 +11,15 @@ export const metadata = {
 
 const MainLayout = ({ children }) => {
   return (
-    <html lang="us">
-      <body>
-        <Navbar />
-        <div>{children}</div>
-        <Footer />
-      </body>
-    </html>
+    <AuthProvider>
+      <html lang="us">
+        <body>
+          <Navbar />
+          <div>{children}</div>
+          <Footer />
+        </body>
+      </html>
+    </AuthProvider>
   )
 }
 
